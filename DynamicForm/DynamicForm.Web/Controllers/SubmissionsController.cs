4ff92d6b86638adbf446d12074544a40e566d87c
Artur Klimov <imbalance1997@gmail.com>
2020-08-13 15:07:57 +0300

    Add some further structure

---
+++ DynamicForm/DynamicForm.Web/Controllers/SubmissionsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicForm.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubmissionsController : ControllerBase
+    {
+        public IActionResult SaveFormSubmission (JsonElement submission)
+        {
+            foreach (var element in submission.EnumerateObject())
+            {
+                var name = element.Name;
+                var value = element.Value.ValueKind;
+            };
+
+            return Ok();
+        }
+    }
+}
